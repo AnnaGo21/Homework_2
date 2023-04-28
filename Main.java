bc1a963aa6bee4579a846583e0085f4c4d329659
Ani <anabellek19@gmail.com>
2023-04-28 17:42:02 +0400

    Modified Main.java file

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Homework;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Mortgage mortgage = new Mortgage();
+
+        mortgage.printMortgage();
+        mortgage.printPaymentSchedule();
+    }
+}
